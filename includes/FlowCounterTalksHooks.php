db04a3860d8f0dbf3de1408447514f35fbeb534f
Solene <solene.moins@gmail.com>
2018-01-08 16:40:04 +0100

    Erreur de l'appel au hook

--- includes/FlowCounterTalksHooks.php
+++ includes/FlowCounterTalksHooks.php
@@ -1,12 +1,45 @@
 <?php
-use FlowCounterTalksHooks;
+namespace FlowCounterTalks;
+use FauxRequest;
+use ApiMain;
 
 class Hook {
 
     public static function onSkinTemplateNavigation( &$page, &$content_navigation ) {
-        global $wgUser;
-  		var_dump($page->getTitle()->getText());
-        var_dump($content_navigation);
+
+        global $wgContLang, $wgUser;
+        $mediaString = strtolower( $wgContLang->getNsText( NS_FILE ) );
+        //$title = $mediaString . ':' . $filename;
+
+
+        $pageTitle = $page ->getTitle();
+        $title = $pageTitle -> getFullText();
+
+       //$text = "\n[[" . $category . "]]";
+        $wgEnableWriteAPI = true;
+        $params = new FauxRequest(array (
+            'action' => 'flow',
+            'submodule'=> 'view-topiclist',
+            'page' => 'Discussion:' .  $title,
+        ), true, $_SESSION );
+//         var_dump($params);
+//         die();
+
+        $enableWrite = true;
+        try {
+            $api = new ApiMain( $params, $enableWrite );
+            $api->execute();
+            if ( defined( 'ApiResult::META_CONTENT' ) ) {
+                $data = $api->getResult()->getResultData();
+            } else {
+                $data = &$api->getResultData();
+            }
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }
+
+        return $mediaString;
+
 
     }
 }
