b5b7cb102893f74fab22a1601390649ab27793cd
Solene <solene.moins@gmail.com>
2018-01-09 16:28:25 +0100

    Mise en place de la fausse api avec le compteur

--- includes/FlowCounterTalksHooks.php
+++ includes/FlowCounterTalksHooks.php
@@ -5,41 +5,31 @@ use ApiMain;
 
 class Hook {
 
-    public static function onSkinTemplateNavigation( &$page, &$content_navigation ) {
+    public static function onSkinTemplateNavigation( &$content_navigation, array &$links  ) {
 
-        global $wgContLang, $wgUser;
-        $mediaString = strtolower( $wgContLang->getNsText( NS_FILE ) );
-        //$title = $mediaString . ':' . $filename;
+        global $wgContLang, $wgUser, $wgTitle;
 
+        $title = $wgTitle->getText();
 
-        $pageTitle = $page ->getTitle();
-        $title = $pageTitle -> getFullText();
-
-       //$text = "\n[[" . $category . "]]";
         $wgEnableWriteAPI = true;
         $params = new FauxRequest(array (
             'action' => 'flow',
             'submodule'=> 'view-topiclist',
-            'page' => 'Discussion:' .  $title,
+            'page' => 'Discussion:' . $title,
         ), true, $_SESSION );
-//         var_dump($params);
-//         die();
+
 
         $enableWrite = true;
-        try {
-            $api = new ApiMain( $params, $enableWrite );
-            $api->execute();
-            if ( defined( 'ApiResult::META_CONTENT' ) ) {
-                $data = $api->getResult()->getResultData();
-            } else {
-                $data = &$api->getResultData();
-            }
-        } catch (\Exception $e) {
-            var_dump($e->getMessage());
+        $api = new ApiMain( $params, $enableWrite );
+        $api->execute();
+        if ( defined( 'ApiResult::META_CONTENT' ) ) {
+            $data = $api->getResult()->getResultData();
+
+        } else {
+            $data = &$api->getResultData();
         }
-
-        return $mediaString;
-
+        $counterTalk = count($data['flow']['view-topiclist']['result']['topiclist']['roots']);
+        $links['namespaces']['talk']['count']=$counterTalk;
 
     }
 }
